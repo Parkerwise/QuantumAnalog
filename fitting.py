39bbf6905ba6be8cde25b4ee1cd010115a925ca1
Parkerwise <wiseparker03@gmail.com>
2024-02-29 11:29:25 -0600

    sliced data to fit sweep

--- fitting.py
+++ fitting.py
@@ -12,13 +12,21 @@ columns=["time","pos"]
 df=pd.read_csv("TEK0004.CSV",usecols=[3,4],names=["time","amp"])
 time=np.array(df.time)
 amp=np.array(df.amp)
+start_index=np.where(time==0)[0][0]
+end_index=np.where(time==0.4)[0][0]
+
+time=time[start_index:end_index]
+amp=amp[start_index:end_index]
 peak_indices = signal.find_peaks(amp,width=20)
 peak_indices=peak_indices[0]
 peak_count = len(peak_indices) # the number of peaks in the array
-print(peak_count)
-print(peak_indices)
-plt.plot(df.time,df.amp)
+
+plt.plot(time,amp)
 peaks_time=[time[peak_indices[i]] for i in range(len(peak_indices))]
 peaks_amp=[amp[[peak_indices[i]]] for i in range(len(peak_indices))]
+
+#get index starting at 0.0 and ending at 0.4 s
+start_index=np.where(time==0)
+end_index=np.where(time==0.4)
 plt.plot(peaks_time,peaks_amp,"o")
 plt.show()
